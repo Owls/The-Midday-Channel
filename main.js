b15ed5a4b5bd6b949ebbee87e54a2dc1b1636de0
Hunter <hunter@Nest.local>
2021-01-31 16:08:53 -0500

    v2

--- main.js
+++ main.js
@@ -45,3 +45,25 @@ $(document).ready(buildTimeline);
 $(document).on('click', SELECTOR_SWITCHER_TV, function() {
     toggleSwitcherTV();
 });
+
+// Adds Sound to pressing Power Button 
+const powerbutton = document.querySelector(".powerbutton");
+
+//TV is OFF by Default
+let tvon = true;
+const gamesound = new Audio("./bkpause.mp3");
+gamesound.loop = true
+const poweron = new Audio("./turnon.mp3"); 
+
+powerbutton.addEventListener("click", () => 
+{
+ poweron.play();
+ if(tvon)
+ {
+   tvon = false;
+   gamesound.pause();
+ }else 
+ {
+  gamesound.play();
+ }
+})
