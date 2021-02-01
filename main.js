85d4f58daf1bdb9d0992a22381afe6c6e2c0ffdc
Hunter <hunter@Nest.local>
2021-01-31 21:51:38 -0500

    DV-03

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 var SELECTOR_SCREEN_ELEMENT = '.content';
 var SELECTOR_SWITCHER_TV = '.powerbutton';
-
+ 
 var isTurnedOn = false;
-
+ 
 var timeline;
-
-function buildTimeline() 
-{
+const gamesound = new Audio("./bkpause.mp3");
+gamesound.loop = true
+gamesound.volue = 0.3;
+const poweron = new Audio("./turnon.mp3");
+ 
+function buildTimeline() {
     timeline = new TimelineMax({
         paused: false
     });
-
+ 
     timeline
         .to(SELECTOR_SCREEN_ELEMENT, .2, {
             width: '100vw',
@@ -24,47 +27,29 @@ function buildTimeline()
             background: '#ffffff'
         });
 }
-
-function toggleSwitcherTV() 
-{
+ 
+function toggleSwitcherTV() {
     if (isTurnedOn) {
         timeline.restart();
+        gamesound.pause();
     }
-
+ 
     if (!isTurnedOn) {
         timeline.reverse();
+        poweron.play();
+        gamesound.play();
     }
-
+ 
     isTurnedOn = !isTurnedOn;
 }
-
+ 
 // Initialize
 $(document).ready(buildTimeline);
-
+ 
 // Bindings
 $(document).on('click', SELECTOR_SWITCHER_TV, function() {
     toggleSwitcherTV();
 });
-
+ 
 // Adds Sound to pressing Power Button 
 const powerbutton = document.querySelector(".powerbutton");
-
-//TV is OFF by Default
-let tvon = true;
-const gamesound = new Audio("./bkpause.mp3");
-gamesound.loop = true
-const poweron = new Audio("./turnon.mp3"); 
-gamesound.volume = (0.3);
-
-powerbutton.addEventListener("click", () => 
-{
- poweron.play();
- if(tvon)
- {
-   tvon = false;
-   gamesound.pause();
- }else 
- {
-  gamesound.play();
- }
-})
