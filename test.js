75e94b528e60e550b3de6927424adf782950203b
Hunter <hunter@Nest.local>
2021-02-11 19:23:31 -0500

    DV-0.9.1

---
+++ test.js
@@ -0,0 +1,10 @@
+// alert('The Developer of this Project is an Idiot, and has not finished this page.');
+// alert('One day, it may be finished but for now, it isnt, sorry.');
+// alert('Still, while youre here, maybe you can help.');
+//
+// var testQuestion = prompt('Would you tell me your greatest secret?');
+//
+// alert('Really? Are you sure? Well... thats pretty silly. But I guess I can let it go this once.');
+// alert('Just kidding, your answer: "' + testQuestion + '" will never be forgotten.');
+// alert('Now loading...');
+
