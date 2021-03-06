d4156ed4a691fc6082a1bca88d6508b7b0a56a40
Owls <mordobird@gmail.com>
2021-03-06 17:55:41 -0500

    DV-0.9.7-10.7 Updates to Home Page.

--- Home/JS/home.js
+++ Home/JS/home.js
@@ -15,4 +15,67 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+function smoothScroll(target, time) {
+    var margin = ($('#main-header').outerHeight() - 1);
+    if (!time) { time = '1000'; }
 
+    if (target === 'toTop') {
+        $('html,body').animate({
+            scrollTop: 0
+        }, time);
+    }
+    else {
+        $('html,body').animate({
+            scrollTop: target.offset().top - margin
+        }, time);
+    }
+}
+
+// SCROLL DOWN ARROR BUTTON
+
+var count = 0;
+
+$('#scroll').on("click", function(){
+
+    var sections = $('section');
+
+    //+55px to offset the margin-top
+    var $this = $(this),
+        top = ($this.offset().top - $(window).scrollTop()) + 55,
+        right = $this.offset().right;
+
+
+    $this.css({
+        position: 'fixed',
+        top: top,
+        right: right
+    })
+        .animate({
+            right: '5%',
+            top: '90%'
+        }, 600)
+        .addClass('clicked');
+
+    var target;
+
+    // END OF ARRAY HAS ALEADY BEEN REACHED
+    if (count > (sections.length -1) ) {
+        count = -1;
+
+        smoothScroll('toTop');
+        $this.removeClass('rotate');
+    }
+    // JUST REACHED END OF ARRAY
+    else {
+        target = $(sections[count]);
+        if (count === (sections.length -1)) {
+            $this.addClass('rotate');
+        }
+        smoothScroll(target);
+    }
+
+    count++;
+
+    $(this).find('.arrow-bounce').removeClass('arrow-bounce');
+
+});
